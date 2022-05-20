1d955e13444c6f4b65b688e6db4529b5494c6479
Derek Huang <djh458@stern.nyu.edu>
2022-05-19 21:57:49 -0400

    CMakeLists.txt: add compiler flags, debug/release setting, tree.h/tree.c: add decls and defs for functions to create/free gen_tree and gen_tree children

--- c/include/pdcip/structures/tree.h
+++ c/include/pdcip/structures/tree.h
@@ -23,31 +23,58 @@ typedef struct gen_tree_ {
 gen_tree *
 gen_tree_malloc(double, size_t, gen_tree **);
 
-void
-gen_tree_free_(gen_tree *, bool);
+/**
+ * Allocate a `gen_tree` instance on the heap with zero children.
+ *
+ * @param v `double` value to set the `gen_tree` node with
+ * @returns `gen_tree *`
+ */
+#define gen_tree_malloc_default(v) gen_tree_malloc(v, 0, NULL)
 
 /**
  * Free a `gen_tree` instance on the heap, but not its children.
  *
  * @param t `gen_tree *` pointer to `gen_tree` to free
+ * @returns `void`
  */
-#define gen_tree_free(t) gen_tree_free_(t, false)
+#define gen_tree_free(t) free(t)
+
+void
+gen_tree_set_children(gen_tree *, size_t, gen_tree **);
 
 /**
- * Free a `gen_tree` instance on the heap and free its direct children.
+ * Reset the children of a `gen_tree *` without freeing the existing children.
  *
- * Use with caution if the children are dereferenced somewhere else!
+ * `n_children` is set to zero and the children are set to `NULL`.
  *
- * @param t `gen_tree *` pointer to `gen_tree` to free
+ * @param t `gen_tree *` pointer to `gen_tree` whose children we reset
  */
-#define gen_tree_free_deep(t) gen_tree_free_(t, true)
+#define get_tree_reset_children(t) gen_tree_set_children(t, 0, NULL)
+
+void
+gen_tree_free_children_(gen_tree *, bool);
 
 /**
- * Allocate a `gen_tree` instance on the heap with zero children.
+ * Free the direct children of a `gen_tree *`.
  *
- * @param v `double` value to set the `gen_tree` node with
+ * @param t `gen_tree *` whose direct children we wish to free
+ * @returns `void`
  */
-#define gen_tree_malloc_default(v) gen_tree_malloc(v, 0, NULL)
+#define gen_tree_free_children(t) gen_tree_free_children_(t, false)
+
+/**
+ * Free the entire child subtree of a `gen_tree *`.
+ *
+ * @param t `gen_tree *` whose child subtree we wish to free
+ * @returns `void`
+ */
+#define gen_tree_free_children_deep(t) gen_tree_free_children_(t, true)
+
+void
+gen_tree_free_deep(gen_tree *);
+
+gen_tree **
+gen_tree_make_children(size_t, double *);
 
 /**
  * A binary tree for numeric data.
@@ -59,12 +86,6 @@ typedef struct binary_tree_ {
 } binary_tree;
 
 /*
-void
-gen_tree_set_children(gen_tree *, size_t, gen_tree **);
-
-gen_tree **
-gen_tree_make_children(size_t, double *);
-
 binary_tree *
 binary_tree_insert(binary_tree *, double);
 */
--- c/src/structures/tree.c
+++ c/src/structures/tree.c
@@ -25,7 +25,7 @@ gen_tree_malloc(double value, size_t n_children, gen_tree **children)
 {
   // n_children can only be zero if children is NULL
   assert((n_children == 0 && !children) || (n_children > 0 && children));
-  gen_tree *tree = malloc(sizeof(gen_tree));
+  gen_tree *tree = (gen_tree *) malloc(sizeof(gen_tree));
   tree->value = value;
   tree->n_children = n_children;
   tree->children = children;
@@ -33,19 +33,81 @@ gen_tree_malloc(double value, size_t n_children, gen_tree **children)
 }
 
 /**
- * Free a `gen_tree` instance on heap, optionally also freeing its children.
+ * Set the children of a `gen_tree *` without freeing the existing children.
  *
- * @param tree `gen_tree *` pointer to `gen_tree` to free
- * @param deep `bool` where `true` also frees children, `false` does not.
+ * If `n_children` is zero, `children` must be `NULL` and `tree->children` will
+ * be set to `NULL` in order to keep the `gen_tree` state consistent.
+ *
+ * @param tree `gen_tree *` tree whose children we will set
+ * @param n_children `size_t` number of new children
+ * @param children `gen_tree **` pointing to `gen_tree *` new children
  */
 void
-gen_tree_free_(gen_tree *tree, bool deep)
+gen_tree_set_children(gen_tree *tree, size_t n_children, gen_tree **children)
 {
-  if (deep) {
+  assert(tree && (children || (n_children == 0 && !children)));
+  tree->n_children = n_children;
+  tree->children = children;
+}
+
+/**
+ * Free the children of a `gen_tree`.
+ *
+ * @param tree `gen_tree *` whose children we will free
+ * @param deep `bool` where if `true` we free the entire child subtree, while
+ *    if `false` we only free the direct children of `tree`.
+ */
+void
+gen_tree_free_children_(gen_tree *tree, bool deep)
+{
+  if (tree->children) {
     for (size_t i = 0; i < tree->n_children; i++) {
-      free(tree->children[i]);
+      if (deep) {
+        gen_tree_free_children_deep(tree->children[i]);
+      }
+      gen_tree_free(tree->children[i]);
     }
     free(tree->children);
   }
+  tree->n_children = 0;
+  tree->children = NULL;
+}
+
+/**
+ * Free a `gen_tree` instance on the heap and free its child subtree.
+ *
+ * Use with caution if the children are dereferenced somewhere else!
+ *
+ * @param t `gen_tree *` pointer to `gen_tree` to free
+ * @returns `void`
+ */
+void
+gen_tree_free_deep(gen_tree *tree)
+{
+  gen_tree_free_children(tree);
   free(tree);
+}
+
+/**
+ * Convenience function that allocates `gen_tree` children on the heap.
+ *
+ * If `n` is 0, then `values` is ignored and `NULL` is returned.
+ *
+ * @param n number of `gen_tree *` children in array
+ * @param values `double *` of values to populate children with
+ * @returns `gen_tree **` to `n` `gen_tree *`
+ */
+gen_tree **
+gen_tree_make_children(size_t n, double *values)
+{
+  // hack to add a message to the assert
+  assert(values && "values must not be NULL");
+  if (!n) {
+    return NULL;
+  }
+  gen_tree **children = (gen_tree **) malloc(n * sizeof(gen_tree *));
+  for (size_t i = 0; i < n; i++) {
+    children[i] = gen_tree_malloc_default(values[i]);
+  }
+  return children;
 }
