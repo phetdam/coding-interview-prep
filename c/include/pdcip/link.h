a4dd9c62db49e1fa7ee5ee86fbb97ab2e2bed641
Derek Huang <djh458@stern.nyu.edu>
2022-06-13 02:10:41 -0400

    pdcip/link.h: add prelim decls for n_next, n_links for single_link

--- c/include/pdcip/link.h
+++ c/include/pdcip/link.h
@@ -8,6 +8,8 @@
 #ifndef PDCIP_LINK_H_
 #define PDCIP_LINK_H_
 
+#include <stddef.h>
+
 /**
  * A simple single-linked list node implementation.
  */
@@ -15,6 +17,12 @@ typedef struct single_link {
   double value;
   struct single_link *next;
 } single_link;
+
+size_t
+single_link_n_next(const single_link *);
+
+size_t
+single_link_n_links(const single_link *);
 
 /**
  * A simple double-linked list node implementation.
