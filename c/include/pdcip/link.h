679eacd0717797318e879890e2420ae2c32cd3bc
Derek Huang <djh458@stern.nyu.edu>
2022-06-19 21:46:34 -0400

    pdcip/link.h: add free_p_links, count_n_p macros and their specializations, link.c: use macros for free and n_next, n_prev functions

--- c/include/pdcip/link.h
+++ c/include/pdcip/link.h
@@ -45,11 +45,81 @@ single_link_malloc(double, single_link *);
  */
 #define single_link_free(l) free(l)
 
+/**
+ * Free the `p` links of a linked object.
+ *
+ * That is, if a linked object has the `p` attribute, keep freeing the `p`
+ * links until the last `p` link is seen to be `NULL`.
+ *
+ * @param t link type, ex. `single_link`
+ * @param h `t *` head of the linked list
+ * @param p attribute name, ex. `next`
+ * @returns void
+ */
+#define free_p_links(t, h, p) \
+  { \
+    t *cur = h->p; \
+    t *p = NULL; \
+    while (cur) { \
+      p = cur->p; \
+      t ## _free(cur); \
+      cur = p; \
+    } \
+  }
+
+/**
+ * Free the `prev` links of a linked object.
+ *
+ * @param t link type, ex. `double_link`
+ * @param h `t *`head of the linked list
+ * @returns void
+ */
+#define free_prev_links(t, h) free_p_links(t, h, prev)
+
+/**
+ * Free the `next` links of a linked object.
+ *
+ * @param t link type, ex. `single_link`
+ * @param h `t *` head of the linked list
+ * @returns void
+ */
+#define free_next_links(t, h) free_p_links(t, h, next)
+
 void
 single_link_free_next_links(single_link *);
 
 void
 single_link_free_deep(single_link *);
+
+/**
+ * Count `p` links starting from head link and bind to name `n_p`.
+ *
+ * @param t link type, ex. `double_link`
+ * @param h `t *` head or link in linked list
+ * @param p attribute name, ex. `next`, so bound name is `n_next`
+ */
+#define count_n_p(t, h, p) \
+  t *cur = h->p; \
+  size_t n_ ## p = 0; \
+  while (cur) { \
+    (n_ ## p)++; \
+  }
+
+/**
+ * Count `prev` links starting from head link and bind to name `n_prev`.
+ *
+ * @param t link type, ex. `double_link`
+ * @param h `t *` head or link in linked list
+ */
+#define count_n_prev(t, h) count_n_p(t, h, prev)
+
+/**
+ * Count `next` links starting from head link and bind to name `n_next`.
+ *
+ * @param t link type, ex. `double_link`
+ * @param h `t *` head or link in linked list
+ */
+#define count_n_next(t, h) count_n_p(t, h, next)
 
 size_t
 single_link_n_next(single_link *);
@@ -65,5 +135,49 @@ typedef struct double_link {
   struct double_link *prev;
   struct double_link *next;
 } double_link;
+
+double_link *
+double_link_malloc(double, double_link *, double_link *);
+
+/**
+ * Allocate a `double_link *` with no next or prev links.
+ *
+ * @param v `double` value the `double_link *` should take
+ * @returns `double_link *`
+ */
+#define double_link_malloc_default(v) double_link_malloc(v, NULL, NULL);
+
+/**
+ * Allocate a `double_link *` with `NAN` value and no next or prev links.
+ *
+ * @returns `double_link *`
+ */
+#define double_link_malloc_empty() double_link_malloc(NAN, NULL, NULL);
+
+/**
+ * Free a `double_link *` without freeing any next or prev links.
+ *
+ * @param l `double_link *` to free
+ * @returns void
+ */
+#define double_link_free(l) free(l)
+
+void
+double_link_free_prev_links(double_link *);
+
+void
+double_link_free_next_links(double_link *);
+
+void
+double_link_free_deep(double_link *);
+
+size_t
+double_link_n_next(double_link *);
+
+size_t
+double_link_n_prev(double_link *);
+
+size_t
+double_link_n_links(double_link *);
 
 #endif  /* PDCIP_LINK_H_ */
--- c/src/link.c
+++ c/src/link.c
@@ -14,7 +14,7 @@
  * Allocate a `single_link *`.
  *
  * @param value `double` value the `single_link *` should take, can be `NAN`
- * @param next `single_link *` next node
+ * @param next `single_link *` next node, can be `NULL`
  */
 single_link *
 single_link_malloc(double value, single_link *next)
@@ -35,6 +35,7 @@ single_link_malloc(double value, single_link *next)
 void
 single_link_free_next_links(single_link *head)
 {
+  /*
   single_link *cur = head->next;
   single_link *next = NULL;
   while (cur) {
@@ -43,6 +44,8 @@ single_link_free_next_links(single_link *head)
     cur = next;
   }
   head->next = NULL;
+  */
+ free_next_links(single_link, head);
 }
 
 /**
@@ -65,11 +68,14 @@ single_link_free_deep(single_link *head)
 size_t
 single_link_n_next(single_link *head)
 {
+  /*
   single_link *cur = head->next;
   size_t n_next = 0;
   while (cur) {
     n_next++;
   }
+  */
+  count_n_next(single_link, head);
   return n_next;
 }
 
@@ -82,4 +88,84 @@ size_t
 single_link_n_links(single_link *head)
 {
   return 1 + single_link_n_next(head);
+}
+
+/**
+ * Allocate a `double_link *`.
+ *
+ * @param value `double` value the `double_link *` should take, can be `NAN`
+ * @param prev `double_link *` prev node, can be `NULL`
+ * @param next `double_link *` next node, can be `NULL`
+ */
+double_link *
+double_link_malloc(double value, double_link *prev, double_link *next)
+{
+  double_link *head = (double_link *) malloc(sizeof(double_link));
+  head->value = value;
+  head->prev = prev;
+  head->next = next;
+  return head;
+}
+
+/**
+ * Free all the prev links in the linked list starting with `link->prev`.
+ *
+ * The `prev` link of `link` will then be set to `NULL`.
+ *
+ * @param link `double_link *` in a of the linked list
+ */
+void
+double_link_free_prev_links(double_link *link)
+{
+ free_prev_links(double_link, link);
+}
+
+/**
+ * Free all the next links in the linked list starting with `link->next`.
+ *
+ * The `next` link of `link` will then be set to `NULL`.
+ *
+ * @param head `double_link *` head of the linked list
+ */
+void
+double_link_free_next_links(double_link *link)
+{
+ free_next_links(double_link, link);
+}
+
+/**
+ * Free a `double_link *` and all its prev and next links.
+ *
+ * @param head `double_link *` link of the linked list
+ */
+void
+double_link_free_deep(double_link *link)
+{
+  double_link_free_prev_links(link);
+  double_link_free_next_links(link);
+  double_link_free(link);
+}
+
+/**
+ * Return number of `prev` links that come before `link`.
+ *
+ * @param link `double_link *` head of the linked list
+ */
+size_t
+double_link_n_prev(double_link *link)
+{
+  count_n_prev(double_link, link);
+  return n_prev;
+}
+
+/**
+ * Return number of `next` links that come after `link`.
+ *
+ * @param link `double_link *` head of the linked list
+ */
+size_t
+double_link_n_next(double_link *link)
+{
+  count_n_next(double_link, link);
+  return n_next;
 }
